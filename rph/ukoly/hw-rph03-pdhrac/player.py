c5dd625b19f64e49058c660f8a8bec83bafe316f
Helena <helena.spoustova@gmail.com>
2025-10-19 22:41:14 +0200

    done rph03

--- rph/ukoly/hw-rph03-pdhrac/player.py
+++ rph/ukoly/hw-rph03-pdhrac/player.py
@@ -1,33 +1,39 @@
 import random
 
-def opposite_move(move):
-    if move == 'R':
-        return 'P'
-    elif move == 'P':
-        return 'S'
-    elif move == 'S':
-        return 'R'
-    else:
-        print("Spatny vstup!")
+payoff_matice = (((4,4),(1,6)), ((6,1),(2,2)))
 
 class MyPlayer():
-    ''' blablabla '''
-    def __init__(self, memlength, weights=None):
-        self.memlength = memlength
+    ''' Hrac spolupracuje poprve a vzdy kdyz i protihrac, jinak 1:2 C:D.'''
+    def __init__(self, payoff_matice, iterations=None):
+        self.payoff_matice = payoff_matice
+        self.iterations = iterations
+        self.memlength = 50
         self.history = [None] * self.memlength
+        self.my_history = [None] * self.memlength
+        self.first_move = True
 
-        if weights is None:
-            self.weights = [1,1,1]
-        else: 
-            self.weights = weights
-
-    def select_move(self):
-        if None in self.history:
-            return random.choices('R', 'P', 'S'), self.weights
+    def select_move(self): 
+        if self.first_move: # Pokud jde o první kolo tak vždy spoulpracuje
+            self.first_move = False
+            return True
+        
+        last_opp_move = self.history[0]
+        
+        if last_opp_move:
+            return True
         else:
-            random_opp_move = random.choice(self.history)
-            return opposite_move(random_opp_move)
+            rand_num = random.randint(1,3) # Poměr cooperate : defect je po defectu protihráče 1:2
+            return rand_num == 3
 
-    def record_last_moves(self, opp_move):
+    def record_last_moves(self, my_move, opp_move):
+        if type(opp_move) != bool:
+            print(f"Špatný vstup!")
+            return
+        
         self.history.insert(0, opp_move)
-        del self.history[-1]
+        if len(self.history) >= self.memlength:
+            del self.history[-1]
+
+        self.my_history.insert(0, my_move) # Zatím nevyužíváme 
+        if len(self.my_history) >= self.memlength:
+            del self.my_history[-1]
